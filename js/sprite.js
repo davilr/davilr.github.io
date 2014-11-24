1b58a9ebc8064e8dce6eb5850ac4d10bd1d5efd8
davilr <wilsendavil@Wilsens-MacBook-Air-2.local>
2014-11-24 10:46:32 +0700

    copied WEBSITE content

---
+++ js/sprite.js
@@ -0,0 +1,158 @@
+// JavaScript Document
+
+
+$(function(){
+		//1
+		   $('#navigation li a').append('<span class="hover"></span>')
+		   
+		   $('#navigation li a').hover(function() {
+	        
+		// Stuff that happens when you hover on + the stop()
+		$('.hover', this).stop().animate({
+			'opacity': 1
+			}, 400,'easeOutSine')
+
+
+	
+	},function() {
+	
+		// Stuff that happens when you unhover + the stop()
+		$('.hover', this).stop().animate({
+			'opacity': 0
+			}, 400, 'easeOutQuad')
+	
+	})
+	
+	$('.hover', this).stop().animate({
+			'opacity': 0
+			}, 0, 'easeOutQuad')
+
+
+		//2
+		   $('#navigation2 li a').append('<span class="hover"></span>')
+		   
+		   $('#navigation2 li a').hover(function() {
+	        
+		// Stuff that happens when you hover on + the stop()
+		$('.hover', this).stop().animate({
+			'opacity': 1
+			}, 400,'easeOutSine')
+
+
+	
+	},function() {
+	
+		// Stuff that happens when you unhover + the stop()
+		$('.hover', this).stop().animate({
+			'opacity': 0
+			}, 400, 'easeOutQuad')
+	
+	})
+		$('.hover', this).stop().animate({
+			'opacity': 0
+			}, 0, 'easeOutQuad')
+
+		//3
+		   $('#navigation3 li a').append('<span class="hover"></span>')
+		   
+		   $('#navigation3 li a').hover(function() {
+	        
+		// Stuff that happens when you hover on + the stop()
+		$('.hover', this).stop().animate({
+			'opacity': 1
+			}, 400,'easeOutSine')
+
+
+	
+	},function() {
+	
+		// Stuff that happens when you unhover + the stop()
+		$('.hover', this).stop().animate({
+			'opacity': 0
+			}, 400, 'easeOutQuad')
+	
+	})
+		$('.hover', this).stop().animate({
+			'opacity': 0
+			}, 0, 'easeOutQuad')
+
+		//4
+		   $('#navigation4 li a').append('<span class="hover"></span>')
+		   
+		   $('#navigation4 li a').hover(function() {
+	        
+		// Stuff that happens when you hover on + the stop()
+		$('.hover', this).stop().animate({
+			'opacity': 1
+			}, 400,'easeOutSine')
+
+
+	
+	},function() {
+	
+		// Stuff that happens when you unhover + the stop()
+		$('.hover', this).stop().animate({
+			'opacity': 0
+			}, 400, 'easeOutQuad')
+	
+	})
+		$('.hover', this).stop().animate({
+			'opacity': 0
+			}, 0, 'easeOutQuad')
+
+		//5
+		   $('#navigation5 li a').append('<span class="hover"></span>')
+		   
+		   $('#navigation5 li a').hover(function() {
+	        
+		// Stuff that happens when you hover on + the stop()
+		$('.hover', this).stop().animate({
+			'opacity': 1
+			}, 400,'easeOutSine')
+
+
+	
+	},function() {
+	
+		// Stuff that happens when you unhover + the stop()
+		$('.hover', this).stop().animate({
+			'opacity': 0
+			}, 400, 'easeOutQuad')
+	
+	})
+		$('.hover', this).stop().animate({
+			'opacity': 0
+			}, 0, 'easeOutQuad')
+
+		//6
+		   $('#navigation6 li a').append('<span class="hover"></span>')
+		   
+		   $('#navigation6 li a').hover(function() {
+	        
+		// Stuff that happens when you hover on + the stop()
+		$('.hover', this).stop().animate({
+			'opacity': 1
+			}, 400,'easeOutSine')
+
+
+	
+	},function() {
+	
+		// Stuff that happens when you unhover + the stop()
+		$('.hover', this).stop().animate({
+			'opacity': 0
+			}, 400, 'easeOutQuad')
+	
+	})
+		$('.hover', this).stop().animate({
+			'opacity': 0
+			}, 0, 'easeOutQuad')
+
+
+
+
+		   });
+		   
+		   
+		   
+	
