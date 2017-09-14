b086caf6831667aaa3aea79db139feab2e5d4cee
Wilsen Davil <davil@Wilsens-MacBook-Pro.local>
2017-09-14 16:52:58 +0800

    CP Up - Del

--- dev/prototypes/js/scripts-2.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-  $(".switchboard .switch").click(function() { 
-	  if ($(".switchboard .board").hasClass("active")) {
-		$(".switchboard .board").removeClass("active");
-	  } else {
-		$(".switchboard .board").addClass("active");
-	  }
-	});
-});
